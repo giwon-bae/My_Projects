c3c0e432389a8009cdec2c0d61eae5d9fd9b554b
giwon-bae <79121621+giwon-bae@users.noreply.github.com>
2022-11-11 00:16:03 +0900

    Update 22-11-11
    made player1 move

--- Push&Pull/Assets/Scripts/Player1_Controller.cs
+++ Push&Pull/Assets/Scripts/Player1_Controller.cs
@@ -6,9 +6,15 @@ public class Player1_Controller : MonoBehaviour
 {
     [SerializeField] GameObject TargetObj;
 
+    public float speed = 3f;
+
+    private float xAxis;
+    private Rigidbody2D playerRigid;
+    private Vector2 moveVec;
+
     void Start()
     {
-        
+        playerRigid = GetComponent<Rigidbody2D>();
     }
 
     void Update()
@@ -17,5 +23,21 @@ public class Player1_Controller : MonoBehaviour
         {
             TargetObj.transform.Translate(new Vector2(0,2f));
         }
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void GetInput()
+    {
+        xAxis = Input.GetAxisRaw("Horizontal");
+    }
+
+    void Move()
+    {
+        moveVec = new Vector2(xAxis * speed, playerRigid.velocity.y);
+        playerRigid.velocity = moveVec;
     }
 }
