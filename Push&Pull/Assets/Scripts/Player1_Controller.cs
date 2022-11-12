5195e6815661fb4f17adb9db335b3ed3b3166fc2
giwon-bae <79121621+giwon-bae@users.noreply.github.com>
2022-11-12 13:51:17 +0900

    Update 22-11-12
    made player1 jump & player2 move, jump

--- Push&Pull/Assets/Scripts/Player1_Controller.cs
+++ Push&Pull/Assets/Scripts/Player1_Controller.cs
@@ -7,8 +7,12 @@ public class Player1_Controller : MonoBehaviour
     [SerializeField] GameObject TargetObj;
 
     public float speed = 3f;
+    public float jumpForce = 5f;
+    public int maxJumpCount = 1;
 
     private float xAxis;
+    private bool jDown;
+    private int jumpCount = 0;
     private Rigidbody2D playerRigid;
     private Vector2 moveVec;
 
@@ -19,10 +23,11 @@ public class Player1_Controller : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetButtonDown("Push"))
-        {
-            TargetObj.transform.Translate(new Vector2(0,2f));
-        }
+        GetInput();
+        //if (Input.GetButtonDown("Push"))
+        //{
+        //    TargetObj.transform.Translate(new Vector2(0,2f));
+        //}
     }
 
     private void FixedUpdate()
@@ -33,11 +38,26 @@ public class Player1_Controller : MonoBehaviour
     void GetInput()
     {
         xAxis = Input.GetAxisRaw("Horizontal");
+        jDown = Input.GetButton("Jump1");
     }
 
     void Move()
     {
         moveVec = new Vector2(xAxis * speed, playerRigid.velocity.y);
         playerRigid.velocity = moveVec;
+
+        if (jDown && jumpCount < maxJumpCount)
+        {
+            playerRigid.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            jumpCount++;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.contacts[0].normal.y > 0.7f)
+        {
+            jumpCount = 0;
+        }
     }
 }
--- Push&Pull/Assets/Scripts/Player2_Controller.cs
+++ Push&Pull/Assets/Scripts/Player2_Controller.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class Player2_Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject TargetObj;
+
+    public float speed = 3f;
+    public float jumpForce = 5f;
+    public int maxJumpCount = 1;
+
+    private float xAxis;
+    private bool jDown;
+    private int jumpCount = 0;
+    private Rigidbody2D playerRigid;
+    private Vector2 moveVec;
+
     void Start()
     {
-        
+        playerRigid = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        GetInput();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void GetInput()
+    {
+        xAxis = Input.GetAxisRaw("Horizontal2");
+        jDown = Input.GetButton("Jump2");
+    }
+
+    void Move()
+    {
+        moveVec = new Vector2(xAxis * speed, playerRigid.velocity.y);
+        playerRigid.velocity = moveVec;
+
+        if (jDown && jumpCount < maxJumpCount)
+        {
+            playerRigid.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            jumpCount++;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.contacts[0].normal.y > 0.7f)
+        {
+            jumpCount = 0;
+        }
     }
 }
